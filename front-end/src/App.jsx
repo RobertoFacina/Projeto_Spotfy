4d4f73c24feb1a2f95d51ee11c08ac094f8184fe
RobertoFacina <robertosfn1998@gmail.com>
2025-05-15 20:07:52 -0300

    Formatação da estrutura

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,7 +11,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/artists" element={<Artists />} />
